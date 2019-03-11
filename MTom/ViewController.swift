b65577480324236b27e3848cb062333afbe099f7
Me That's Right <hackerman@peters-mac.local>
2019-03-11 03:05:45 -0700

    Initial Commit

---
+++ MTom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MTom
+//
+//  Created by Peter Kowalski on 11/03/2019.
+//  Copyright © 2019 Peter Kowalski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
